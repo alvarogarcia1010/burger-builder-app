d67f639c164ca514bd7075cab5754e8cc52204fe
alvarogarcia1010 <00111317@uca.edu.sv>
2020-03-13 12:42:32 -0600

    Add Container Burger

---
+++ src/containers/BurgerBuilder/BurgerBulder.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  bacon: 0.6,
+  meat: 1.3
+}
+
+class BurgerBulder extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      ingredients: {
+        salad: 0,
+        cheese: 0,
+        bacon: 0,
+        meat: 0
+      },
+      totalPrice: 0
+    }
+  }
+
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCounted = oldCount + 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCounted;
+
+    const oldPrice = this.state.totalPrice;
+    const priceAddition = INGREDIENT_PRICES[type]
+    const newPrice = oldPrice + priceAddition
+    this.setState({ingredients: updatedIngredients, totalPrice:newPrice})
+
+  }
+
+  removeIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+
+    if(oldCount<=0)
+    {
+      return;
+    }
+
+    const updatedCounted = oldCount - 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCounted;
+
+    const oldPrice = this.state.totalPrice;
+    const priceDeduction = INGREDIENT_PRICES[type]
+    const newPrice = oldPrice - priceDeduction
+    this.setState({ingredients: updatedIngredients, totalPrice:newPrice})
+  }
+
+  render() 
+  {
+    const disabledInfo = { ...this.state.ingredients};
+
+    for(let key in disabledInfo)
+    {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients}/>
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemove={this.removeIngredientHandler}
+          disabled={disabledInfo}
+          price={this.state.totalPrice}
+        />
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBulder;
