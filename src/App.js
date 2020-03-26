ff9e40ee2e125529219be1f9226e7a9c13ee49f5
alvarogarcia1010 <00111317@uca.edu.sv>
2020-03-25 18:24:57 -0600

    Seccion 12

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBulder'
 import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
+import Orders from './containers/Orders/Orders';
 
 function App() 
 {
@@ -11,6 +12,7 @@ function App()
       <Layout>
         <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
           <Route exact path="/" component={BurgerBuilder} />
         </Switch>
       </Layout>
